14b7b0fe03dfd1e53dceaa0b0fa16455c053412c
Sean OReilly <soreilly424@gmail.com>
2021-07-08 11:52:33 -0500

    removed getLandingPage

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,21 +1,19 @@
-import { useState } from 'react'
-import Hamburger from './Hamburger'
-import NavLinks from './NavLinks'
-import NavLogo from './NavLogo'
-import Link from 'next/link'
-import MobileNavbar from './MobileNavbar'
-import { imageBuilder } from 'lib/sanity'
+import { useState } from "react";
+import Hamburger from "./Hamburger";
+import NavLinks from "./NavLinks";
+import NavLogo from "./NavLogo";
+import MobileNavbar from "./MobileNavbar";
 
 export default function Navbar({ prints, photographs }) {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   const toggle = () => {
-    setOpen(!open)
-  }
+    setOpen(!open);
+  };
 
   return (
     <section
       className={`flex fixed ${
-        open ? '' : 'md:static bg-white md:bg-opacity-95'
+        open ? "" : "md:static bg-white md:bg-opacity-95"
       } xl:flex-col lg:pt-16 pt-10 items-center xl:mb-0 justify-between z-50  w-screen gap-4`}
     >
       <div className="xl:self-center pl-10 md:pl-20 xl:pr-0 z-50">
@@ -27,5 +25,5 @@ export default function Navbar({ prints, photographs }) {
         <MobileNavbar open={open} toggle={toggle} />
       </nav>
     </section>
-  )
+  );
 }
--- pages/photographs/index.js
+++ pages/photographs/index.js
@@ -1,30 +1,28 @@
-import React from 'react'
-import { getPhotographs, getAllArt, getLandingPage } from '../../lib/api'
-import HorizontalScroll from '@/components/HorizontalScroll'
-import Layout from '@/components/Layout'
+import React from "react";
+import { getPhotographs, getAllArt } from "../../lib/api";
+import HorizontalScroll from "@/components/HorizontalScroll";
+import Layout from "@/components/Layout";
 
-export default function galleries({ content, allArt, landingPage }) {
-  const images = content.map((c) => c.featureImage)
-  const titles = content.map((c) => c.title)
-  const photographs = allArt.filter((art) => art.category === 'photographs')
-  const prints = allArt.filter((art) => art.category === 'prints')
-  const artPieces = landingPage[0].artPieces.map((art) => art.asset)
+export default function galleries({ content, allArt }) {
+  const images = content.map((c) => c.featureImage);
+  const titles = content.map((c) => c.title);
+  const photographs = allArt.filter((art) => art.category === "photographs");
+  const prints = allArt.filter((art) => art.category === "prints");
 
   return (
-    <Layout artPieces={artPieces} prints={prints} photographs={photographs}>
+    <Layout prints={prints} photographs={photographs}>
       <main>
         <HorizontalScroll content={content} images={images} titles={titles} />
       </main>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps({ preview = false }) {
-  const content = await getPhotographs(preview)
-  const allArt = await getAllArt(preview)
-  const landingPage = await getLandingPage(preview)
+  const content = await getPhotographs(preview);
+  const allArt = await getAllArt(preview);
   return {
-    props: { preview, content, allArt, landingPage },
+    props: { preview, content, allArt },
     revalidate: 1,
-  }
+  };
 }
