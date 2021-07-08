d8c4d9cdde569f825bf48f29b7a1ef8c1fc360dc
Sean OReilly <soreilly424@gmail.com>
2021-07-08 12:36:28 -0500

    made changes to the index page

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,59 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import { getAllArt } from '@/lib/api'
-import { imageBuilder } from '@/lib/sanity'
-import Layout from '@/components/Layout'
-import HorizontalScroll from '@/components/HorizontalScroll'
-import TestEnv from '@/components/TestEnv'
+import Link from "next/link";
+import Image from "next/image";
+import { imageBuilder } from "@/lib/sanity";
+import { getAllArt } from "@/lib/api";
+import Layout from "@/components/Layout";
 
-export default function galleries({ content }) {
-  const images = content.map((c) => c.featureImage)
-  const titles = content.map((c) => c.title)
-  const photographs = content.filter((art) => art.category === 'photographs')
-  const prints = content.filter((art) => art.category === 'prints')
+export default function galleries({ art }) {
+  const photographs = art
+    .filter((art) => art.category === "photographs")
+    .map((item) => item.artPieces)
+    .flat();
+  const prints = art
+    .filter((art) => art.category === "prints")
+    .map((item) => item.artPieces)
+    .flat();
+  const randomPhoto =
+    photographs[Math.floor(Math.random() * photographs.length)];
+  const randomPrint = prints[Math.floor(Math.random() * prints.length)];
 
   return (
-    <Layout photographs={photographs} prints={prints}>
-      <main className="pt-10 md:pt-0 lg:flex hidden">
-        <HorizontalScroll content={content} images={images} titles={titles} />
-      </main>
-      <main>
-        <TestEnv />
-        <section className="lg:hidden flex flex-col my-10">
-          <article className="grid grid-cols-2 gap-4">
-            {images.map((image, idx) => (
-              <Link
-                key={idx}
-                href={`/${content[idx].category}/${content[idx].slug.current}`}
-              >
-                <a>
-                  <div
-                    className={`flex relative shadow-2xl px-8 justify-center w-min lg:w-max bg-gray-100 uppercase tracking-widest m-auto`}
-                  >
-                    <h1 className="italic">{titles[idx]}</h1>
-                  </div>
-                  {image !== undefined && (
-                    <Image
-                      src={imageBuilder(image.asset).url()}
-                      alt={`Adam Finkelston`}
-                      width={500}
-                      height={500}
-                    />
-                  )}
-                </a>
-              </Link>
-            ))}
-          </article>
-        </section>
+    <Layout>
+      <main className="flex my-20 xl:mx-40 mx-10 justify-center gap-10">
+        <div className="flex flex-col relative h-96 w-full transform hover:scale-105 cursor-pointer transition-all duration-300 ease-linear group">
+          <h1 className="z-50 relative mx-auto bottom-10 text-2xl font-black uppercase text-gray-300 group-hover:text-gray-700 transition-all duration-200 ease-linear">
+            View Photography
+          </h1>
+          <Image
+            src={imageBuilder(randomPhoto.asset).url()}
+            alt={"Photography by Adam Finkelston"}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+          />
+        </div>
+
+        <div className="flex flex-col relative h-96 w-full transform hover:scale-105 cursor-pointer transition-all duration-300 ease-linear group">
+          <h1 className="z-50 relative mx-auto bottom-10 text-2xl font-black uppercase text-gray-300 group-hover:text-gray-700 transition-all duration-200 ease-linear">
+            View Prints
+          </h1>
+          <Image
+            src={imageBuilder(randomPrint.asset).url()}
+            alt={"Prints by Adam Finkelston"}
+            layout="fill"
+            objectFit="cover"
+            objectPosition="center"
+          />
+        </div>
       </main>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps({ preview = false }) {
-  const content = await getAllArt(preview)
-
+  const art = await getAllArt(preview);
   return {
-    props: { preview, content },
+    props: { preview, art },
     revalidate: 1,
-  }
+  };
 }
